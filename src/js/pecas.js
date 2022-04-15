403a10a5304e6d0d09248373598f3e2b2e8919d6
Gabriel Torrezan <36551976+Scandiuzzii@users.noreply.github.com>
2022-04-15 18:30:30 -0300

    Ajuste espaçamento de peças

--- src/js/pecas.js
+++ src/js/pecas.js
@@ -1,4 +1,3 @@
-
 class Pecas {
     constructor(id, player) {
         this.id = id;
@@ -12,9 +11,11 @@ class Pecas {
         newTokenElement.id = id
         newTokenElement.style.background = player == 1 ? 'rgb(41, 230, 85, 0.9)' : 'rgb(75, 145, 250, 0.9)'
         let left = player == 1 ? 3 : 23 + tabuleiro.offsetWidth / document.documentElement.clientHeight * 100
-        newTokenElement.style.left = left + Math.random() * 12 + 'vh'
-        // newTokenElement.style.top = right + Math.random() * 12 + 'vh'
-        newTokenElement.style.top = '0vh'
+        newTokenElement.style.marginBlockStart = player == 1 ? left + Math.random() * 60 + 'vh' : left + Math.random() * 30 + 'vh'
+        newTokenElement.style.top = player == 1 ? '5vh' : 'vh'
+            // newTokenElement.style.left = '6vh'
+
+
         document.body.appendChild(newTokenElement)
 
         // let finalTop = tabuleiro.clientHeight * (0.34 + 0.32 * Math.random()) / document.documentElement.clientHeight * 100
